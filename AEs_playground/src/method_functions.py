5bdb045a030a3c34fe2dc5b372bbd07667894dd4
Alessandro Longhi <aalelonghi@apollo.tnw.tudelft.nl>
2025-02-26 11:32:59 +0100

    variance decoder is now common, added dataset 1D advection

--- AEs_playground/src/method_functions.py
+++ AEs_playground/src/method_functions.py
@@ -88,10 +88,10 @@ def l_regularization_latent_space(lambda_regularization, Auto_Encoder, latent_sp
     
     elif Auto_Encoder == 'VAE':
         regularization_latent = tc.mean(KL_divergence(means, log_variances)) * lambda_regularization
-        if tc.isnan(regularization_latent):
-            print('l_regularization_latent_space')
-            print(log_variances)
-            exit()
+        #if tc.isnan(regularization_latent):
+        #    print('l_regularization_latent_space')
+        #    print(log_variances)
+        #    exit()
     return regularization_latent
 
 def KL_divergence(means,log_variances):
@@ -115,15 +115,15 @@ def reconstruction_loss_VAE(inp, target, log_variances, dim_input):
     if dim_input > 1:
         inp = inp.flatten(start_dim=-dim_input)
         target = target.flatten(start_dim=-dim_input)
-        log_variances = log_variances.flatten(start_dim=-dim_input)
+        #log_variances = log_variances.flatten(start_dim=-dim_input)
     
-    log_variances = tc.clamp(log_variances, min=-10, max=10)
+    log_variances = tc.clamp(log_variances, min=-6, max=6)
     variances = tc.exp(log_variances) + eps 
     loss = tc.mean(tc.sum((inp - target)**2, dim=-1) / (2 * variances) + inp.size(-1) * 0.5 * log_variances)
-    if tc.isnan(loss):
-        print('reconstruction_loss_VAE encountered NaN')
-        print(log_variances)
-        exit()
+    #if tc.isnan(loss):
+    #    print('reconstruction_loss_VAE encountered NaN')
+    #    print(log_variances)
+    #    exit()
     return loss
 
 
